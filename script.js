69ddfcd60f308520b092d9803941c472a952436a
Victoria Adebona <vadebona@uci.edu>
2017-10-31 10:08:22 -0700

    scrollify stuff

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+$(function aboutSection() {
+   $.scrollify({
+       section: '#about',
+   });
+});
 
+$(function projectsSection() {
+   $.scrollify({
+       section: '#projects',
+   });
+});
+
+$(function contactSection() {
+   $.scrollify({
+       section: '#contact',
+   });
+});
+
+$(function sourcesSection() {
+   $.scrollify({
+       section: '#sources',
+   });
+});
