bdad7b9ebc9bc3de00d88312d7ca3fedce978285
Victoria Adebona <vadebona@uci.edu>
2017-11-12 21:13:32 -0800

    even more changes, added parallax

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-$(document).ready(function() {
-    // W3Schools Smooth Scrolling:
-    $("a").on('click', function(event) {
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
 
-        if (this.hash !== "") {
-            event.preventDefault();
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-            var hash = this.hash;
+      // Store hash
+      var hash = this.hash;
 
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function(){
-                window.location.hash = hash;
-            });
-        }
-    });
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 });
