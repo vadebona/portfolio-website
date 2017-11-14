58e43bd4bfa2f78c240ae52c07a2cbd5820c5e28
Victoria Adebona <vadebona@uci.edu>
2017-11-13 22:26:07 -0800

    basically final changes

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// W3Schools Fullscreen nav: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_overlay
+function openNav() {
+    document.getElementById("nav-menu").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("nav-menu").style.width = "100%";
+}
+
 $(document).ready(function(){
     // W3Schools Smooth Scrolling:
     $("a").on('click', function(event) {
@@ -16,12 +25,6 @@ $(document).ready(function(){
     }
   });
 
-    // W3Schools Fullscreen nav:
-    function openNav() {
-        document.getElementById("nav").style.width = "100%";
-    }
-
-    function closeNav() {
-        document.getElementById("nav").style.width = "0%";
-    }
 });
+
+
